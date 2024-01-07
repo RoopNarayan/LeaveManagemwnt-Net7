ed12c6b88c57ceb73e432f4571d637e40817bc30
J S <J S@ROOP>
2024-01-07 18:20:39 +0000

    Add project files.

---
+++ LeaveManagemwnt.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagemwnt.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
